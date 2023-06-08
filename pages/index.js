0a960e0c1ef118c5547a5b3cc8f5cb27d40aa881
DhanasekarA-18 <dhanasekard848@gmail.com>
2023-06-08 15:31:35 +0530

    git-inti

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+      <Head>
+        <title>FOOD APP</title>
+        <meta name="description" content="foodApp by Dhanasekar A" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+     
+  )
+}
