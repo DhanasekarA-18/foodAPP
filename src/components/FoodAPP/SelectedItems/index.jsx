0587e9f546046d53fa98f6939f304305993d98fd
DhanasekarA-18 <dhanasekard848@gmail.com>
2023-06-10 00:02:26 +0530

    build-fix

--- src/components/FoodAPP/SelectedItems/index.jsx
+++ src/components/FoodAPP/SelectedItems/index.jsx
@@ -1,21 +1,32 @@
-import React from 'react';
-import styles from './styles.module.scss';
-import Button from '../Button';
+import React from "react";
+import styles from "./styles.module.scss";
+import Button from "../Button";
 
-const SelectedItems = ({ currentlySelected ,totalAmount,setShowSelected,setShow}) => {
+const SelectedItems = ({
+  currentlySelected,
+  totalAmount,
+  setShowSelected,
+  setShow,
+}) => {
+  const foodItemHeadings = [
+    "Item Name",
+    "Item Cost",
+    "Selected Count",
+    "Item Total Cost",
+  ];
+  const customStyle = {
+    backgroundColor: "#022b50",
+  };
 
-  const foodItemHeadings =['Item Name','Item Cost','Selected Count','Item Total Cost'];
- const customStyle={
-    backgroundColor:'#022b50'
- }
- const handleBack=()=>{
+  const handleBack = () => {
     setShowSelected(false);
- }
+  };
 
- const handleNext=()=>{
+  const handleNext = () => {
     setShow(true);
     setShowSelected(false);
- }
+  };
+
   return (
     <>
       <header className={styles.selectedheader}>
@@ -23,27 +34,31 @@ const SelectedItems = ({ currentlySelected ,totalAmount,setShowSelected,setShow}
       </header>
       <main className={styles.selectedItemList}>
         <section>
-            <table>
-                <thead>
-                   {foodItemHeadings.map((title,index)=>(
-                    <th key={index}>{title}</th>
-                   ))}
-                </thead>
-                <tbody>
-          {Object.values(currentlySelected).map((item, index) => (
-           <tr key={index}>
-            <td>{item?.name}</td>
-            <td>{item?.cost}</td>
-            <td>{item?.selectedCount}</td>
-            <td>{item?.cost * item?.selectedCount}</td>
-           </tr>
-          ))}
-          </tbody>
+          <table>
+            <thead>
+              {foodItemHeadings.map((title, index) => (
+                <th key={index}>{title}</th>
+              ))}
+            </thead>
+            <tbody>
+              {Object.values(currentlySelected).map((item, index) => (
+                <tr key={index}>
+                  <td>{item?.name}</td>
+                  <td>{item?.cost}</td>
+                  <td>{item?.selectedCount}</td>
+                  <td>{item?.cost * item?.selectedCount}</td>
+                </tr>
+              ))}
+            </tbody>
           </table>
         </section>
         <section className={styles.buttonContainer}>
-        <Button title={`Edit`} customStyle={...customStyle} handleClick={handleBack}/>
-        <Button title={`Pay ₹${totalAmount}`} handleClick={handleNext}/>
+          <Button
+            title={`Edit`}
+            customStyle={customStyle}
+            handleClick={handleBack}
+          />
+          <Button title={`Pay ₹${totalAmount}`} handleClick={handleNext} />
         </section>
       </main>
     </>
