eab8d38480bf45a28aedcc9229ee53e3500860a6
Dhanasekar A <83594375+DhanasekarA-18@users.noreply.github.com>
2024-10-13 19:42:53 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: "export",
   env: {
     RAZORPAY_KEY: "rzp_test_VliP97Q1oGSeTa",
     RAZORPAY_KEY_SECRETE: "pay215qWADlIT5HsySzKrXZu",
